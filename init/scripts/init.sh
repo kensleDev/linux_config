a72cef206641b7ffab1c90b2996983295ff5cd28
kensleDev <kensledev@gmail.com>
2020-11-03 17:44:42 +0000

    updated init script

--- init/scripts/init.sh
+++ init/scripts/init.sh
@@ -1,4 +1,5 @@
 # ------------------------------------ COMMAND LINE
+HOME_DIR=/home/kd
 
 # install nvm
 
@@ -7,24 +8,24 @@ sudo apt-get install -y zsh &&
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
 
 # if the script stoppped when installing oh my zsh resume from here
-curl -L git.io/antigen > ~/.oh-my-zsh/plugins/antigen.zsh &&
+curl -L git.io/antigen > $HOME_DIR/.oh-my-zsh/plugins/antigen.zsh &&
 
 # Copy Dotfiles
-cp ../dofiles/.zshrc /home/kd
-cp ../dotfiles/.alias /home/kd
-cp ../dotfiles/.gitconfig /home/kd
+cp ../dotfiles/.zshrc $HOME_DIR
+cp ../dotfiles/.alias $HOME_DIR
+cp ../dotfiles/.gitconfig $HOME_DIR
 
 # Clean up zsh
-echo "zsh" >> ~/.bashrc &&
+echo "zsh" >> $HOME_DIR/.bashrc &&
 exec bash -l
 
 # For updating the files in this gist
-git clone https://gist.github.com/kensleDev/e27ddee966af517a712e9e2f8043eedc /home/kd/.config/init/
-mv  /home/kd/.config/init/e27ddee966af517a712e9e2f8043eedc /home/kd/.config/init/gist
+git clone https://gist.github.com/kensleDev/e27ddee966af517a712e9e2f8043eedc $HOME_DIR/.config/init/
+mv  $HOME_DIR/.config/init/e27ddee966af517a712e9e2f8043eedc $HOME_DIR/.config/init/gist
 
 # nvm, node, yarn 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash &&
-/home/kd/.nvm/nvm install 12.13.0 &&
+$HOME_DIR/.nvm/nvm install 12.13.0 &&
 npm i -g yarn
 
 
