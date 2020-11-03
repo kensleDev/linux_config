0ad0c8458eaa694c4a277dbaaa5fee82938fac2f
kensleDev <kensledev@gmail.com>
2020-11-03 18:00:48 +0000

    Update init.sh

--- init/scripts/init.sh
+++ init/scripts/init.sh
@@ -1,8 +1,6 @@
 # ------------------------------------ COMMAND LINE
 HOME_DIR=/home/kd
 
-# install nvm
-
 # install zsh
 sudo apt-get install -y zsh &&
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
@@ -15,17 +13,15 @@ cp ../dotfiles/.zshrc $HOME_DIR
 cp ../dotfiles/.alias $HOME_DIR
 cp ../dotfiles/.gitconfig $HOME_DIR
 
+# nvm, node, yarn 
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh && source ~/.zshrc &&
+nvm install 12.13.0 &&
+npm i -g yarn
+
 # Clean up zsh
 echo "zsh" >> $HOME_DIR/.bashrc &&
 exec bash -l
 
-# For updating the files in this gist
-git clone https://gist.github.com/kensleDev/e27ddee966af517a712e9e2f8043eedc $HOME_DIR/.config/init/
-mv  $HOME_DIR/.config/init/e27ddee966af517a712e9e2f8043eedc $HOME_DIR/.config/init/gist
 
-# nvm, node, yarn 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash &&
-$HOME_DIR/.nvm/nvm install 12.13.0 &&
-npm i -g yarn
 
 
