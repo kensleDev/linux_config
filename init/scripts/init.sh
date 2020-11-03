83dd775d8e17cabe7937a9e5c202190762c212a2
kensleDev <kensledev@gmail.com>
2020-11-03 18:42:47 +0000

    Update init.sh

--- init/scripts/init.sh
+++ init/scripts/init.sh
@@ -15,6 +15,11 @@ cp ../dotfiles/.gitconfig $HOME_DIR
 
 # nvm, node, yarn 
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash &&
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 12.13.0
+npm i -g yarn 
 
 # Clean up zsh
 source ~/.zshrc &&
