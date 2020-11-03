9acd63e8a220a85195e5844b67c23d17aeef2d78
kensleDev <kensledev@gmail.com>
2020-11-03 19:40:31 +0000

    Update init.sh

--- init/scripts/init.sh
+++ init/scripts/init.sh
@@ -1,6 +1,12 @@
 # ------------------------------------ COMMAND LINE
 HOME_DIR=/home/kd
 
+function log() {
+  echo "-------------------------------"
+  echo "-> $1"
+  echo "-------------------------------"
+}
+
 # install zsh
 sudo apt-get install -y zsh &&
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
@@ -12,19 +18,20 @@ curl -L git.io/antigen > $HOME_DIR/.oh-my-zsh/plugins/antigen.zsh &&
 cp ../dotfiles/.zshrc $HOME_DIR
 cp ../dotfiles/.alias $HOME_DIR
 cp ../dotfiles/.gitconfig $HOME_DIR
+log "Copied Dotfiles"
 
 # setup Fonts
 sudo apt-get install fontconfig &&
 mkdir ~/.fonts &&
 cp ~/.config/fonts/VictorMono/TTF/* ~/.fonts &&
-fc-cache -f -c &&
+sudo fc-cache -f -v &&
+log "setup fonts"
 
 # command line tools - vim
-sudo apt-get install ripgrep &&
-sudo apt install bat &&
-sudo apt-get install ctags &&
+sudo apt-get install ripgrep bat ctags -y &&
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf &&
 ~/.fzf/install --all &&
+log "Installed command line tools"
 
 # nvm, node, yarn 
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash &&
@@ -33,6 +40,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 12.13.0
 npm i -g yarn 
+log "Installed node and yarn"
 
 # Clean up zsh
 echo "zsh" >> $HOME_DIR/.bashrc
