8db78d69762a9936dedcd137ad40ad1604c5c06f
kensleDev <kensledev@gmail.com>
2020-11-03 21:21:52 +0000

    Update init.sh

--- init/scripts/init.sh
+++ init/scripts/init.sh
@@ -8,17 +8,17 @@ function log() {
 }
 
 # install zsh
-sudo apt-get install -y zsh &&
+sudo apt-get install -y zsh curl &&
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
 
 # if the script stoppped when installing oh my zsh resume from here
 curl -L git.io/antigen > $HOME_DIR/.oh-my-zsh/plugins/antigen.zsh &&
 
 # copy dotfiles
-cp ../dotfiles/.zshrc $HOME_DIR
-cp ../dotfiles/.alias $HOME_DIR
-cp ../dotfiles/.gitconfig $HOME_DIR
-log "Copied Dotfiles"
+cp ../dotfiles/.zshrc $HOME_DIR &&
+cp ../dotfiles/.alias $HOME_DIR &&
+cp ../dotfiles/.gitconfig $HOME_DIR &&
+log "Copied Dotfiles" &&
 
 # setup Fonts
 sudo apt-get install fontconfig &&
@@ -44,6 +44,14 @@ sudo apt-get update &&
 sudo apt-get install ripgrep -y &&
 log "Installed Ripgrep" &&
 
+# lazy git 
+sudo add-apt-repository -y ppa:lazygit-team/release &&
+sudo apt-get update &&
+sudo apt-get install -y lazygit &&
+
+# lazy docker
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash &&
+
 # Universal Ctags
 git clone https://github.com/universal-ctags/ctags.git &&
 cd ctags &&
@@ -51,17 +59,17 @@ cd ctags &&
 ./configure &&
 make &&
 sudo make install &&
-cd ..
-sudo rm -rf ctags
+cd .. 
+sudo rm -rf ctags &&
 
 # fd-find
 wget https://github.com/sharkdp/fd/releases/download/v8.1.1/fd-musl_8.1.1_amd64.deb &&
 sudo dpkg -i fd-musl_8.1.1_amd64.deb &&
-log "Installed fd"
+log "Installed fd" &&
 
 # Silver Sercher Ag
 sudo apt install -y silversearcher-ag &&
-log "Installed Silver Searcher ag"
+log "Installed Silver Searcher ag" &7
 
 # bat
 wget https://github.com/sharkdp/bat/releases/download/v0.15.4/bat_0.15.4_amd64.deb &&
@@ -74,21 +82,24 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf &&
 ~/.fzf/install --all &&
 log "Installed FZF" &&
 
+
 # vim plug 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-log "Installed Vim Plug"
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &&
+log "Installed Vim Plug" &&
+
+
 
 # nvm, node, yarn 
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash &&
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install 12.13.0
-npm i -g yarn 
-log "Installed node and yarn"
+nvm install 12.13.0 &&
+npm i -g yarn &&
+log "Installed node and yarn" &&
 
 # Clean up zsh
-echo "zsh" >> $HOME_DIR/.bashrc
+echo "zsh" >> $HOME_DIR/.bashrc &&
 exec bash -l
 
