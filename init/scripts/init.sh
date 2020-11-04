b8701c4d8880bd2349c83d2fc10d09a2a733f7a1
kensleDev <kensledev@gmail.com>
2020-11-04 00:07:24 +0000

    Update init.sh

--- init/scripts/init.sh
+++ init/scripts/init.sh
@@ -113,6 +113,10 @@ sudo apt-get install -y ruby-full
 sudo gem install neovim
 log "Installed Ruby"
 
+# perl
+sudo cpan -n Neovim::Ext
+log "Installed perl"
+
 # vim plug 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &&
@@ -124,7 +128,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 12.13.0 &&
-npm i -g yarn &&
+npm i -g yarn neovim &&
 log "Installed node and yarn" &&
 
 # Vim plugins
